3513ea5fce7db74f96fb6f39c273611243e6ef71
Hans5958 <email5958.1@gmail.com>
2021-04-23 18:18:19 +0700

    Credit SAB for commits

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -5,8 +5,8 @@ cd $ACTION_PATH
 cp -r ./tmp/* $GITHUB_WORKSPACE/
 
 cd $GITHUB_WORKSPACE
-git config user.email "33279053+apple502j@users.noreply.github.com"
-git config user.name "apple502j"
+git config user.email "73682299+scratchaddons-bot[bot]@users.noreply.github.com"
+git config user.name "scratchaddons-bot[bot]"
 
 if git status | grep -q "git add"; then
     echo Update available. Pushing to GitHub...
